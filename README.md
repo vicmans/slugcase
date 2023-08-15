e572991307b5841582d298c8ad27886b73d5e68f
Victor <vsanchezl94@gmail.com>
2023-08-15 16:58:06 -0400

    :tada: first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# slugcase
+
+A library to convert a string to slug format
+
+## Installation
+
+```sh
+npm install slugcase
+
+```
+
+## Usage
+
+```js
+import { slugcase } from 'slugcase',
+
+const myString = 'Hola como estas?';
+
+const mySlugString = slugcase(myString);
+
+// mySlugString
+// hola-como-estas
+```
+
+## Roadmap
+
+- [ ] Add Typescript
+- [ ] Add Unit tests
+- [ ] Suport to ESM and CommonJS
+- [ ] Whatever
---
+++ index.js
@@ -0,0 +1,27 @@
+/**
+ * Case a string to slug case
+ * @param {string} str the string 
+ * @returns the slug string
+ */
+const slugcase = (str) => {
+  str = str.trim();
+  str = str.replace(/^\s+|\s+$/g, ''); // trim
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  const from = 'ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;';
+  const to = 'aaaaaeeeeeiiiiooooouuuunc------';
+  for (let i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  str = str
+    .replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+    .replace(/\s+/g, '-') // collapse whitespace and replace by -
+    .replace(/-+/g, '-') // collapse dashes
+    .replace(/^-+/, '')  // Trim - from start of text
+    .replace(/-+$/, ''); // Trim - from end of text
+  return str;
+};
+
+module.exports.slugcase = slugcase;
